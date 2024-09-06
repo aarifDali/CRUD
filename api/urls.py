0a00e4e24eb5b7a88865a2639cdf8e1f0a8e1a74
aarifDali <aliaarif995@gmail.com>
2024-09-06 12:46:03 +0530

    get and post with rest

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ApiOverview, name='home'),
+    path('create/', views.add_items, name='add_items'),
+    path('all/', views.view_items, name='view_items'),
 ]
--- api/views.py
+++ api/views.py
@@ -17,3 +17,32 @@ def ApiOverview(request):
     }
  
     return Response(api_urls)
+
+
+@api_view(['POST'])
+def add_items(request):
+    item = ItemSerializer(data = request.data)
+
+    if Item.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This item already exists')
+    
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response({'message':'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['GET'])
+def  view_items(request):
+
+    if request.query_params:
+        items = Item.objects.filter(**request.query_params.dict())
+    else:
+        items = Item.objects.all()
+
+    
+    if items:
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+    else:
+        return Response({'message':'No item found'}, status=status.HTTP_404_NOT_FOUND)
